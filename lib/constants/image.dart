8891b685f74d662d334a0aee09c3cf92b19fe1c5
zyllus17 <mrfhassan17@gmail.com>
2023-06-20 00:36:28 +0530

    Added pics

---
+++ lib/constants/image.dart
@@ -0,0 +1,11 @@
+class AppImages {
+  AppImages._();
+
+  //Books pics
+  static const String googling = "assets/googling.png";
+  static const String programmerlife = "assets/programmerlife.png";
+  static const String stackoverflow = "assets/stackoverflow.png";
+
+  //Profile pic
+  static const String profile = "assets/profile.jpg";
+}
