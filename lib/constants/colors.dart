1eea78284821acccfd548836073fa3e2ec09be65
zyllus17 <mrfhassan17@gmail.com>
2023-06-20 00:36:18 +0530

    Added colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color yellow = Color(0xffFFE229);
+  static const Color black = Color(0xff000000);
+  static const Color grey = Color(0xff605E5E);
+  static const Color borderGrey = Color(0xffE7E7E7);
+}
